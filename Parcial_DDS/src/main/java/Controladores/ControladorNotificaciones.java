7211473426f6b69efc8e6dab493824ce29cf8cd2
FeroMateo <feromateo@gmail.com>
2021-08-23 15:31:34 -0300

    Notif

---
+++ Parcial_DDS/src/main/java/Controladores/ControladorNotificaciones.java
@@ -0,0 +1,65 @@
+package Controladores;
+
+import com.twilio.Twilio;
+import com.twilio.rest.api.v2010.account.Message;
+import com.twilio.type.PhoneNumber;
+
+public class ControladorNotificaciones {
+    // Find your Account SID and Auth Token at twilio.com/console
+    // and set the environment variables. See http://twil.io/secure
+    public static final String ACCOUNT_SID = "AC41023bd65e9f12d9ae58b997b747c989";
+    public static final String AUTH_TOKEN = "c829bac4037cd46ff78b73611a4997d9";
+
+    public void notificarClienteSMS(Integer id_pedido){
+
+        String mensaje = String.format("PEDIDO NUMERO: %d CONFIRMADO, DEMORA ESTIMADA 10 MINUTOS",id_pedido);
+
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("+541132027257"),
+                new com.twilio.type.PhoneNumber("+19713512077"),
+                mensaje)
+                .create();
+
+        //System.out.println(message.getSid());
+    }
+    public void notificarRepartidorSMS(String direccion,Integer id_pedido){
+
+        String mensaje = String.format("ENVIAR PEDIDO %d A %s EN MENOS DE 10 MINUTOS",id_pedido,direccion);
+
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("+541123732266"),
+                new com.twilio.type.PhoneNumber("+19713512077"),
+                mensaje)
+                .create();
+
+        //System.out.println(message.getSid());
+    }
+    public void notificarClienteWP(Integer id_pedido){
+
+        String mensaje = String.format("PEDIDO NUMERO: %d CONFIRMADO, DEMORA ESTIMADA 10 MINUTOS",id_pedido);
+
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("whatsapp:+5491132027257"),
+                new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+                mensaje)
+                .create();
+
+        //System.out.println(message.getSid());
+    }
+    public void notificarRepartidorWP(String direccion,Integer id_pedido){
+
+        String mensaje = String.format("ENVIAR PEDIDO %d A %s EN MENOS DE 10 MINUTOS",id_pedido,direccion);
+
+        Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
+        Message message = Message.creator(
+                new com.twilio.type.PhoneNumber("whatsapp:+5491123732266"),
+                new com.twilio.type.PhoneNumber("whatsapp:+14155238886"),
+                mensaje)
+                .create();
+
+        //System.out.println(message.getSid());
+    }
+}
---
+++ Parcial_DDS/src/main/java/domain/Prueba.java
@@ -0,0 +1,10 @@
+package domain;
+
+public class Prueba
+{
+    public static void main(String[] args)
+    {
+        Cliente cliente = new Cliente();
+        cliente.comprarGolosinas();
+    }
+}
